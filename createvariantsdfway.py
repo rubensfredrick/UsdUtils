7584619c946b9570487924bc04af713382762805
rubensfredrick <39145982+rubensfredrick@users.noreply.github.com>
2022-12-16 10:51:22 +1100

    Update createvariantsdfway.py
    move function to common.py

--- createvariantsdfway.py
+++ createvariantsdfway.py
@@ -1,22 +1,6 @@
 from collections import namedtuple
 from pxr import Sdf
 
-def sdfCreatePrimAtPath(parent, primPath, specifier=Sdf.SpecifierDef, typeName=""):
-    if isinstance(parent, Sdf.Layer):
-        parent = parent.pseudoRoot  # use the actual layer roots primSpec
-    path = ""
-    for name in str(primPath).split("/"):
-        if not name:
-            continue
-        path += "/{}".format(name)
-        # try getting by path first, then by `name` only (which will work on variant primspecs)
-        primSpec = parent.GetPrimAtPath(path) or parent.nameChildren.get(name)
-        if not primSpec:
-            primSpec = Sdf.PrimSpec(parent, name, specifier, typeName)
-
-        parent = primSpec
-    return parent
-
 def addVariantSetToInstanceName(layer, instanceName, setVar=True)
     VariantValues = namedtuple("VariantValues", ["on", "off"])
     VariantSet = "myVariantSet"
