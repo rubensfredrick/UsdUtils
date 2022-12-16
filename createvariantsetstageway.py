953be4a4792514faf63ea2ce192b1f8a814382bd
rubensfredrick <39145982+rubensfredrick@users.noreply.github.com>
2022-12-16 11:15:37 +1100

    Update createvariantsetstageway.py

--- createvariantsetstageway.py
+++ createvariantsetstageway.py
@@ -1,36 +1,30 @@
 from pxr import Usd
 from . import common
 
-def createVariantSetOInStage(layer)
+def createVariantSetOInStage(layer, subPrimName, myVariantSetName, myVariantName, attribNameValue={})
     stage = Usd.Stage.Open(layer, Usd.Stage.LoadNone)
 
     # Determine state to be written into the fragment.
-    editPrimPath = self.editPrimPath(stage, layer)
+    editPrimPath = stage.GetDefaultPrim().GetPath().AppendChild(subPrimName)
     editPrimSpec = common.sdfCreatePrimAtPath(layer, editPrimPath)
-    editPrimSpec.kind = self.EditPrimKind
-    editPrimSpec.typeName = "ALEdit"
+    editPrimSpec.kind = "edit"
+    editPrimSpec.typeName = "Edit"
 
     # Create stage to create ALEdit prim.
     editPrim = stage.GetPrimAtPath(editPrimPath)
 
     # Ensure a variant set named "animatic" exists.
     variantSets = editPrim.GetVariantSets()
-    variantSet = variantSets.AddVariantSet(self.VariantSetName)
+    variantSet = variantSets.AddVariantSet(myVariantSetName)
 
-    variantSet.AddVariant(self.AnimaticVariantName)
-    variantSet.SetVariantSelection(self.AnimaticVariantName)
+    variantSet.AddVariant(myVariantName)
+    variantSet.SetVariantSelection(myVariantName)
+    
+    # Add attribute if required
     with variantSet.GetVariantEditContext():
-        attribute = editPrim.CreateAttribute(
-            self.IdentifierAttributeName, self.IdentifierAttributeValue, False,
-        )
-        attribute.Set(self.animaticTechVariantUri(layer))
+        for key, val in attribNameValue.items():
+            attribute = editPrim.CreateAttribute(key, val[0], False,)  # val can be attrib type like Sdf.ValueTypeNames.Asset
+            attribute.Set(val[1])  # uri
 
-    variantSet.AddVariant(self.StoryboardVariantName)
-    variantSet.SetVariantSelection(self.StoryboardVariantName)
-    with variantSet.GetVariantEditContext():
-        attribute = editPrim.CreateAttribute(
-            self.IdentifierAttributeName, self.IdentifierAttributeValue, False,
-        )
-        attribute.Set(self.storyboardTechVariantUri(layer))
-
+    # to remove varaint selection
     variantSet.SetVariantSelection("")
